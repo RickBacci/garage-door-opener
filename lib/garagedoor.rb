9487a26e510e86c14899df935efde2601cb1211d
Richard Bacci <brownieelf175@gmail.com>
2014-12-07 09:53:55 -0500

    rename open method

--- lib/garagedoor.rb
+++ lib/garagedoor.rb
@@ -1,28 +1,38 @@
 require 'sinatra'
+
 require 'wiringpi'
 require 'pi_piper'
 include PiPiper
 
-require './on_off'
-require './activate_door'
+
+def activate_door
+  pin = PiPiper::Pin.new(:pin => 17, :direction => :out, :invert => false)
+  pin.off
+  sleep 1
+  pin.on
+end
 
 
+def check_status
+  @gpio = WiringPi::GPIO.new
+  status = @gpio.read(7)
 
+  return "OPEN" if status == 1 
+  "CLOSED"
+end
 
+# def press_button
+#   @gpio = WiringPi::GPIO.new
 
-
-
-
-
-
-
-
-
+#   @gpio.write(0, 0)
+#   sleep 1
+#   @gpio.write(0, 1)
+# end
 
 get '/' do
   erb :index
 end
 
-get '/open' do
-  erb :open  
+get '/activate' do
+  erb :activate  
 end
