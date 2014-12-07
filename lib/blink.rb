2453ca2cefc4e17eabb135f067d392a1acd5b7c2
Rick Bacci <brownieelf175@gmail.com>
2014-12-07 14:01:08 +0000

    switch to pipiper for activate action

--- lib/blink.rb
+++ lib/blink.rb
@@ -1,8 +1,9 @@
 require 'pi_piper'
 include PiPiper
 
-pin = PiPiper::Pin.new(:pin => 17, :direction => :out, :invert => false)
-pin.off
-sleep 1
-pin.on
-
+def activate
+  pin = PiPiper::Pin.new(:pin => 17, :direction => :out, :invert => false)
+  pin.off
+  sleep 1
+  pin.on
+end
--- lib/garagedoor.rb
+++ lib/garagedoor.rb
@@ -1,10 +1,12 @@
 require 'sinatra'
 require 'wiringpi'
-
+require 'pi_piper'
+include PiPiper
 
 # require_relative '/home/pi/garagedoor/lib/on_off'
 # require_relative 'on_off'
 require './on_off'
+require './blink'
 
 get '/' do
   erb :index
